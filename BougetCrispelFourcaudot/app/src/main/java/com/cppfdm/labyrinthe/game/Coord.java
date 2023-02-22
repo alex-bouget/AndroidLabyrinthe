ac53a73ad50f40003fd0838e74d1d043fbb38314
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-02-22 17:42:07 +0100

    Better doc

--- BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/game/Coord.java
+++ BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/game/Coord.java
@@ -1,14 +1,16 @@
 package com.cppfdm.labyrinthe.game;
 
+/**
+ * Class to store and make easy operation on Cartesian coordinates
+ */
 public class Coord {
     private int x;
     private int y;
 
-    /*
-    Generate a coordinate
-    @param x: x element of the Coordinate
-    @param y: y element of the coordinate
-    @return : A Coord object
+    /**
+     * Constructor of Coord object
+     * @param x : the x element of a coordinate
+     * @param y : the y element of a coordinate
      */
     public Coord(int x, int y) {
         this.x = x;
