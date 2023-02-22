83fd1277fa7d63cc4fdc0224c161a4edb5e43f2e
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-02-22 17:39:59 +0100

    Add documentation to Coord constructor

--- BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/game/Coord.java
+++ BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/game/Coord.java
@@ -4,6 +4,12 @@ public class Coord {
     private int x;
     private int y;
 
+    /*
+    Generate a coordinate
+    @param x: x element of the Coordinate
+    @param y: y element of the coordinate
+    @return : A Coord object
+     */
     public Coord(int x, int y) {
         this.x = x;
         this.y = y;
