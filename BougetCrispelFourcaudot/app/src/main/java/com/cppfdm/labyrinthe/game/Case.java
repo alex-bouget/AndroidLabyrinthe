2e47c04ea73934e8d70f0168a50b4b8b1ef52992
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-02-22 17:54:04 +0100

    Add constructors for case class

--- BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/game/Case.java
+++ BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/game/Case.java
@@ -1,5 +1,29 @@
 package com.cppfdm.labyrinthe.game;
 
+/**
+ * Case class who represent a Case in the labyrinth
+ */
 public class Case {
 
+    private Coord pos;
+    Case[] neighbours;
+
+    /**
+     * Constructor of a Case with a Coord object
+     * @param pos : A Coord object for the Case
+     */
+    public Case(Coord pos) {
+        this.pos = pos;
+        this.neighbours = new Case[4];
+    }
+
+    /**
+     * Constructor of a Case with Cartesian coordinates
+     * @param x : x position of the Case
+     * @param y : y position of the Case
+     */
+    public Case(int x, int y) {
+        this.pos = new Coord(x, y); 
+        this.neighbours = new Case[4];
+    }
 }
