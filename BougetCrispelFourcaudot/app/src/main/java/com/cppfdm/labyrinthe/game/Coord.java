91046f8ab0b00396220f300e4ea89dabca910446
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-02-22 17:36:08 +0100

    Add coord constructor

--- BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/game/Coord.java
+++ BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/game/Coord.java
@@ -1,5 +1,11 @@
 package com.cppfdm.labyrinthe.game;
 
 public class Coord {
+    private int x;
+    private int y;
 
+    public Coord(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
