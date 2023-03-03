4b62365805dd37686ad506d81ad1d4d22149ab86
Crispel Clément <clement.crispel@gmail.com>
2023-03-03 11:38:59 +0100

    conflict

--- BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/MainActivity.java
+++ BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/MainActivity.java
@@ -52,33 +52,6 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    /**
-     * Run choose Labyrinthe
-     */
-    public void runLabyrintheChoose() {
-        Intent intent = new Intent();
-        intent.setClass(this, LabyrintheChooserActivity.class);
-        startActivityForResult(intent, LabyrintheChooserActivity.INTENT_ID);
-    }
-
-    /**
-     * Callback of activity
-     *
-     * @param requestCode the id of the activity
-     * @param resultCode the result code of the activity
-     * @param data the intent returned by the activity
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == LabyrintheChooserActivity.INTENT_ID) {
-            if (resultCode == RESULT_OK) {
-                Labyrinth labyrinth = (Labyrinth) Serializer.get(data.getStringExtra("labyrinth"));
-                //TODO
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
     /***
      * Do a left move for the player
      *
