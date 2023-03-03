193e050d01cba620a581fe61fb788db67c49f938
Crispel Clément <clement.crispel@gmail.com>
2023-03-03 11:19:57 +0100

    add doc

--- BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/MainActivity.java
+++ BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/MainActivity.java
@@ -8,6 +8,11 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    /***
+     * Called when the appliction is created
+     *
+     * @param savedInstanceState the instance that the application have to create
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,18 +21,38 @@ public class MainActivity extends AppCompatActivity {
         game.setMinimumHeight(game.getWidth());
     }
 
+    /***
+     * Do a left move for the player
+     *
+     * @param view view where the action come from
+     */
     public void moveLeft(View view){
 
     }
 
+    /***
+     * Do a right move for the player
+     *
+     * @param view view where the action come from
+     */
     public void moveRight(View view){
 
     }
 
+    /***
+     * Do a up move for the player
+     *
+     * @param view view where the action come from
+     */
     public void moveUp(View view){
 
     }
 
+    /***
+     * Do a down move for the player
+     *
+     * @param view view where the action come from
+     */
     public void moveDown(View view){
 
     }
