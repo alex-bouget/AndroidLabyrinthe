d92e96e3037f16736f8a8dd87126ecfec5d68f7f
Crispel Clément <clement.crispel@gmail.com>
2023-03-03 11:33:46 +0100

    merge conflic

--- BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/MainActivity.java
+++ BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cppfdm.labyrinthe;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.View;
@@ -19,8 +21,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         SurfaceView game = (SurfaceView) findViewById(R.id.game);
         game.setMinimumHeight(game.getWidth());
+        runLabyrintheChoose();
     }
 
+    /**
+     * Run choose Labyrinthe
+     */
+    public void runLabyrintheChoose() {
+        Intent intent = new Intent();
+        intent.setClass(this, LabyrintheChooserActivity.class);
+        startActivityForResult(intent, LabyrintheChooserActivity.INTENT_ID);
+    }
+
+    /**
+     * Callback of activity
+     *
+     * @param requestCode the id of the activity
+     * @param resultCode the result code of the activity
+     * @param data the intent returned by the activity
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == LabyrintheChooserActivity.INTENT_ID) {
+            if (resultCode == RESULT_OK) {
+                Labyrinth labyrinth = (Labyrinth) Serializer.get(data.getStringExtra("labyrinth"));
+                //TODO
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
     /***
      * Do a left move for the player
      *
