877a6413c81863884443c25a45234fc94d1ef530
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-03 11:26:16 +0100

    Add the generate neighbours method

--- BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/game/Case.java
+++ BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/game/Case.java
@@ -34,4 +34,28 @@ public class Case {
     public Coord getCoord() {
         return new Coord(this.pos.getX(), this.pos.getY());
     }
+
+    protected void generateNeighbours(Labyrinth laby) {
+        int row = laby.getROW();
+        int col = laby.getCOL();
+
+        // Generate the coordinate of each case's neighbours
+        Coord[] nextTo = new Coord[4];
+        nextTo[0] = new Coord((this.pos.getX()+1) % col, this.pos.getY()); // right
+        nextTo[1] = new Coord((this.pos.getX()-1) % col, this.pos.getY()); // left
+        nextTo[2] = new Coord(this.pos.getX(), (this.pos.getY()-1) % row); // up
+        nextTo[3] = new Coord(this.pos.getX(), (this.pos.getY()+1) % row); // down
+
+        // Add results in this.neighbours
+        int index = 0;
+        for (Coord c : nextTo) {
+            if (Math.abs(c.getX()-this.pos.getX()) + Math.abs(c.getY()-this.pos.getY()) != 1) {
+                Case current = laby.getCase(c);
+                if (current != null) {
+                    this.neighbours[index] = current;
+                    index++;
+                }
+            }
+        }
+    }
 }
