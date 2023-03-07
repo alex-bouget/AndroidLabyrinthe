e925bc85989b0545f707ca1b53ab49472c232b66
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-07 17:33:44 +0100

    Add getter and doc

--- BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/game/Enemy.java
+++ BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/game/Enemy.java
@@ -4,7 +4,19 @@ public class Enemy {
 
     Coord pos;
 
+    /**
+     * Constructor of an enemy
+     * @param coord position of the Enemy
+     */
     public Enemy(Coord coord) {
         this.pos = coord;
+    }
+
+    /**
+     * Getter of pos attribute
+     * @return Coord object pos
+     */
+    public Coord getPos() {
+        return pos;
     }
 }
