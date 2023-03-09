f7dd20e297704cc9c906bff0d92ab726814d3996
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-09 14:42:16 +0100

    Better code and documentation

--- BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/gameView/tileset/GrassTileSet.java
+++ BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/gameView/tileset/GrassTileSet.java
@@ -19,7 +19,9 @@ import java.util.Set;
 public class GrassTileSet extends AbstractTileset {
 
     private final static String ASSETS_PATH = "tiles/grass/";
+    // Store each tile type to not make calculations for each render
     private String[][] casesTiles;
+    private Set<String> allTiles;
 
     public GrassTileSet(View v, Labyrinth laby) {
         super(v);
@@ -28,35 +30,48 @@ public class GrassTileSet extends AbstractTileset {
         try {
             for (String filename : assetManager.list(ASSETS_PATH)) {
                 addBitmap(filename.substring(0, filename.length() - 4), ASSETS_PATH + filename);
-
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Set<String> allTiles = getAllTiles().keySet();
+        allTiles = getAllTiles().keySet();
         casesTiles = new String[laby.getCOL()][laby.getROW()];
         for (int i = 0; i < laby.getCOL(); i++) {
             for (int j = 0; j < laby.getROW(); j++) {
-                if (laby.getCase(new Coord(i, j)) == null) {
-                    casesTiles[i][j] = "water";
-                } else {
-                    String imgPath = "";
-                    for (int offX = -1; offX < 2; offX++) {
-                        for (int offY = -1; offY < 2; offY++) {
-                            imgPath += laby.getCase(new Coord(i + offY, j + offX)) == null ? "0" : "1";
-                        }
-                    }
-                    if (imgPath.equals("111111111")) {
-                        imgPath = "C_" + (int) (new Random().nextInt(12) + 1);
-                    } else if (! allTiles.contains(imgPath)) {
-                        imgPath = this.resolveImgPath(imgPath);
-                    }
-                    casesTiles[i][j] = imgPath;
-                }
+                generateTileType(i, j, laby);
             }
         }
     }
 
+    /**
+     * Generate the tile type for a tile in the laby
+     * @param i X coordinate in the labyrinth
+     * @param j Y coordinate in the labyrinth
+     * @param laby the labyrinth
+     */
+    private void generateTileType(int i, int j, Labyrinth laby) {
+        if (laby.getCase(new Coord(i, j)) == null) {
+            return;
+        }
+        String imgPath = "";
+        for (int offX = -1; offX < 2; offX++) {
+            for (int offY = -1; offY < 2; offY++) {
+                imgPath += laby.getCase(new Coord(i + offY, j + offX)) == null ? "0" : "1";
+            }
+        }
+        if (imgPath.equals("111111111")) {
+            imgPath = "C_" + (int) (new Random().nextInt(12) + 1);
+        } else if (!allTiles.contains(imgPath)) {
+            imgPath = this.resolveImgPath(imgPath);
+        }
+        casesTiles[i][j] = imgPath;
+    }
+
+    /**
+     * Change a non existing img path to an existing img path
+     * @param img the imgPath
+     * @return the new imgPath
+     */
     private String resolveImgPath(String img) {
         StringBuilder res = new StringBuilder(img);
         if (img.charAt(1) == '0' || img.charAt(3) == '0') {
