43a67d4ae7377ffcac94296d2d2edd2781cba9b8
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-09 16:56:49 +0100

    Add the harbor tileset class

---
+++ BougetCrispelFourcaudot/app/src/main/java/com/cppfdm/labyrinthe/gameView/tileset/HarborTileSet.java
@@ -0,0 +1,75 @@
+package com.cppfdm.labyrinthe.gameView.tileset;
+
+import android.content.res.AssetManager;
+import android.view.View;
+
+import com.cppfdm.labyrinthe.game.Case;
+import com.cppfdm.labyrinthe.game.Coord;
+import com.cppfdm.labyrinthe.game.Labyrinth;
+import com.cppfdm.labyrinthe.view.tileset.AbstractTileset;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.Set;
+
+public class HarborTileSet extends AbstractTileset{
+
+    private final static String ASSETS_PATH = "tiles/harbor/";
+    // Store each tile type to not make calculations for each render
+    private String[][] casesTiles;
+    private Set<String> allTiles;
+
+    public HarborTileSet(View v, Labyrinth laby) {
+        super(v);
+        // Add all bitmap
+        AssetManager assetManager = v.getResources().getAssets();
+        try {
+            for (String filename : assetManager.list(ASSETS_PATH)) {
+                addBitmap(filename.substring(0, filename.length() - 4), ASSETS_PATH + filename);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        allTiles = getAllTiles().keySet();
+        casesTiles = new String[laby.getCOL()][laby.getROW()];
+        for (int i = 0; i < laby.getCOL(); i++) {
+            for (int j = 0; j < laby.getROW(); j++) {
+                generateTileType(i, j, laby);
+            }
+        }
+    }
+
+    public void generateTileType(int i, int j, Labyrinth laby) {
+        // handle null case
+        if (laby.getCase(new Coord(i, j)) == null) {return;}
+        // generate good case
+        String imgPath = "";
+        imgPath += laby.getCase(new Coord(i , j -1)) == null ? "0" : "1";
+        imgPath += laby.getCase(new Coord(i -1 , j)) == null ? "0" : "1";
+        imgPath += laby.getCase(new Coord(i +1, j)) == null ? "0" : "1";
+        imgPath += laby.getCase(new Coord(i , j + 1)) == null ? "0" : "1";
+
+        casesTiles[i][j] = imgPath;
+    }
+
+    @Override
+    public String getTilesName(Case aCase) {
+        if (aCase == null) {return null;}
+        return casesTiles[aCase.getCoord().getX()][aCase.getCoord().getY()];
+    }
+
+    @Override
+    public String getExitTilesName() {
+        return "end";
+    }
+
+    @Override
+    public String getStartTilesName() {
+        return "start";
+    }
+
+    @Override
+    public String getBackgroundTilesName() {
+        return "water" + (int)(frame%8/4);
+    }
+}
